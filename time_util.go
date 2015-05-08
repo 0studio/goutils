9724e743ad2a6533a2676741ef80b4bdb33c8df1
jixiuf <jixiuf@gmail.com>
2015-05-08 19:50:53 +0800

    update timeutil

--- time_util.go
+++ time_util.go
@@ -1,5 +1,9 @@
 package goutils
 
+import (
+	"time"
+)
+
 var monthDays = [...]int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 // 获取指定年月一共有多少天
@@ -11,4 +15,18 @@ func GetMonthDayCount(year, month int) int { // month[1~12]
 }
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+// 返回本月1号， 属于今年第几天
+func GetYearDayOfCurrentMonthFirstDay(now time.Time) int {
+	monthTime := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+	now.Month()
+	return monthTime.YearDay()
+}
+
+// 返回本月最后一天， 属于今年第几天
+func GetYearDayOfCurrentMonthLastDay(now time.Time) int {
+	montDayCount := GetMonthDayCount(now.Year(), int(now.Month()))
+	monthEndTime := time.Date(now.Year(), now.Month(), montDayCount, 0, 0, 0, 0, now.Location())
+	return monthEndTime.YearDay()
 }
