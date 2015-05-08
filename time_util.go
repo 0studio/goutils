1af254acfbef5829ec05a2f94041bafd3313433c
jixiuf <jixiuf@gmail.com>
2015-05-08 20:50:02 +0800

    update timeutils

--- time_util.go
+++ time_util.go
@@ -18,15 +18,15 @@ func IsLeapYear(year int) bool {
 }
 
 // 返回本月1号， 属于今年第几天
-func GetYearDayOfCurrentMonthFirstDay(now time.Time) int {
+func GetYearDayOfCurrentMonthFirstDay(now time.Time) int32 {
 	monthTime := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
 	now.Month()
-	return monthTime.YearDay()
+	return int32(monthTime.YearDay())
 }
 
 // 返回本月最后一天， 属于今年第几天
-func GetYearDayOfCurrentMonthLastDay(now time.Time) int {
+func GetYearDayOfCurrentMonthLastDay(now time.Time) int32 {
 	montDayCount := GetMonthDayCount(now.Year(), int(now.Month()))
 	monthEndTime := time.Date(now.Year(), now.Month(), montDayCount, 0, 0, 0, 0, now.Location())
-	return monthEndTime.YearDay()
+	return int32(monthEndTime.YearDay())
 }
